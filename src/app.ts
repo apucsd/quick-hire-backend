a02313a0760158ebe8773bdfc00da3a4a7e6d675
apucsd <apusutradhar77@gmail.com>
2026-03-01 13:38:17 +0600

    remove windston logger

--- src/app.ts
+++ src/app.ts
@@ -18,11 +18,10 @@ app.use(
     })
 );
 //  ============ LOGGER MIDDLEWARE ============
-app.use(Morgan.errorHandler);
-app.use(Morgan.successHandler);
+// app.use(Morgan.errorHandler);
+// app.use(Morgan.successHandler);
 
 //  ============ PARSER MIDDLEWARE ============
-app.use(express.json());
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
