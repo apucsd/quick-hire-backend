93478424e328e1fd5897a3fd2bf4eb16ab1ced84
apucsd <apusutradhar77@gmail.com>
2026-03-01 13:42:20 +0600

    added frontend url for cors issue

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(rateLimiter);
 
 app.use(
     cors({
-        origin: ['http://localhost:3001', 'http://localhost:3000', 'http://localhost:5173'],
+        origin: ['http://localhost:3001', 'https://quick-hire-liard.vercel.app', 'http://localhost:5173'],
         credentials: true,
     })
 );
